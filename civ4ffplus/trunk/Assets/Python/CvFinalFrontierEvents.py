f9f868baf07aa6dfa758c9813041097d3f66c7bb
prgmtc01 <prgmtc01@82c2dd29-be50-40db-bdd9-e5630c664fec>
2012-04-30 23:48:55 +0000

    Added vanilla CvGameUtils and blank CvFinalFrontierEvents.py to fix installer-related issues

---
+++ civ4ffplus/trunk/Assets/Python/CvFinalFrontierEvents.py
@@ -0,0 +1,6 @@
+#This file is no longer necessary!
+#We now use the BUG event manager system, in FinalFrontierEvents.py
+#Changes to this file will do absolutely nothing, so don't even bother
+
+#TC01 and God-Emperor
+#Final Frontier Plus
